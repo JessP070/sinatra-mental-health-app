d3bd7b1ccd87e8687a2bf3c7f201232eacf5e3c1
JessP070 <jcpereira070@gmail.com>
2020-08-08 13:35:48 -0700

    added route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
+    if logged_in?
+      redirect "/users/#{current_user}"
+    else
+      erb :welcome
+    end
     erb :welcome
   end
 
