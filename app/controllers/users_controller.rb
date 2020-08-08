044b31ffb214a23167165dffde7b0b069979ec85
JessP070 <jcpereira070@gmail.com>
2020-08-07 20:21:26 -0700

    authenticate/redirect user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,26 @@ class UsersController < ApplicationController
     end
 
     post '/login' do
+        @user = User.find_by(email: params[:id])
+
+        if @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            puts session
+            redirect "users/#{@user.id}"
+            
+        else 
+
+        end
     end
     
     
     get 'signup' do
 
     end 
+
+    get '/users/:id' do
+        "user page"
+    end
 
 
 end
