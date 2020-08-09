10859ead3705434280646e0a8d7c1243cd28b26f
JessP070 <jcpereira070@gmail.com>
2020-08-08 19:09:23 -0700

    add post entry

--- app/controllers/health_entries_controller.rb
+++ app/controllers/health_entries_controller.rb
@@ -6,6 +6,17 @@ class HealthEntriesController < ApplicationController
     end
 
     post '/health_entries' do
+        if !logged_in?
+            redirect '/'
+        end
         
+        if params[:content] != ""
+            @health_entry = HealthEntry.create(content: params[:content], user_id: current_user.id)
+            redirect 'health_entries/#{@health_entry.id}'
+        
+        else 
+            redirect :'/health_entries/new'
+        end
+
     end
 end
