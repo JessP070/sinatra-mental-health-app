e2e296a3198cae215307f51734d6d6926c30552d
JessP070 <jcpereira070@gmail.com>
2020-08-08 17:05:04 -0700

    add health entries controller

---
+++ app/controllers/health_entries_controller.rb
@@ -0,0 +1,11 @@
+class HealthEntriesController < ApplicationController
+    
+    get '/health_entries/new' do
+        erb :'/health_entries/new'
+
+    end
+
+    post '/health_entries' do
+        
+    end
+end
