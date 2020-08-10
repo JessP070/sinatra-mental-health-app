b70ae209783b60c9f844ff82de0d9fd447c774b8
JessP070 <jcpereira070@gmail.com>
2020-08-09 23:59:33 -0700

    method to authorize to edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,6 +29,9 @@ class ApplicationController < Sinatra::Base
      @current_user ||= User.find_by(id: session[:user_id])
     end
 
+    def authorized_to_edit?(health_entry)
+      health_entry.user == current_user
+
   end
 
 end
